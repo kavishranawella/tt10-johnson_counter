e1e0556b29aeffd1dc6217bb23bd80d8370968cd
Kavish Ranawella <66011681+kavishranawella@users.noreply.github.com>
2025-03-27 16:27:21 -0400

    Adjusted drive code

--- test/test.py
+++ test/test.py
@@ -18,40 +18,39 @@ async def test_loopback(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
 
-    dut.ui_in.value = 5
+    dut.ui_in.value = 0
     dut.uio_in.value = 0
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut.ui_in.value[7] = 1
+    dut.ui_in.value = (1 << 7)
 
     temp = 0
 
     for i in range(128):
         temp = dut.uo_out.value
-        dut.ui_in.value[0:7] = i
-        await ClockCycles(dut.clk, 2)
-        if(i==6):
-            assert (dut.uo_out.value[0:7] == i), (
-                f"Test failed at iteration {i}: "
-                f"Expected bit 0:7 to be {i}, but got {dut.uo_out.value[0:7]}. "
-                f"Full output: {dut.uo_out.value}, Input: {dut.ui_in.value}"
-            )
-            assert (dut.uo_out.value[7] == ~temp[0]), (
-                f"Test failed at iteration {i}: "
-                f"Expected bit 7 to be {~temp[0]}, but got {dut.uo_out.value[7]}. "
-                f"Full output: {dut.uo_out.value}, Previous output: {temp}"
-            )
+        dut.ui_in.value = ((dut.uio_in.value & 0x80) | (i & 0x7F))
+        await ClockCycles(dut.clk, 1)
+        assert (dut.uo_out.value[0:7] == i), (
+            f"Test failed at iteration {i}: "
+            f"Expected bit 0:7 to be {i}, but got {dut.uo_out.value[0:7]}. "
+            f"Full output: {dut.uo_out.value}, Input: {dut.ui_in.value}"
+        )
+        assert (dut.uo_out.value[7] == ~temp[0]), (
+            f"Test failed at iteration {i}: "
+            f"Expected bit 7 to be {~temp[0]}, but got {dut.uo_out.value[7]}. "
+            f"Full output: {dut.uo_out.value}, Previous output: {temp}"
+        )
 
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
 
-    dut.ui_in.value[7] = 0
+    dut.ui_in.value = 0
 
     for i in range(128):
         temp = dut.uo_out.value
-        dut.ui_in.value[0:7] = i
+        dut.ui_in.value = ((dut.uio_in.value & 0x80) | (i & 0x7F))
         await ClockCycles(dut.clk, 1)
         assert (dut.uo_out.value[0:7] == temp[1:8]), (
             f"Test failed at iteration {i}: "
